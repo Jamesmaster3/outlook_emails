b68ae12b786a31f2dbcee720c70a93bfd139a4e3
Jamesmaster3 <120321282+Jamesmaster3@users.noreply.github.com>
2023-01-07 14:05:33 +0100

    Updated to pull input from csv file

--- emails_via_outlook.py
+++ emails_via_outlook.py
@@ -1,50 +1,42 @@
 import win32com.client
 from pathlib import Path
 from string import Template
-import csv
+import pandas as pd
+import pathlib
+
+path = pathlib.Path('name_list.csv')
 
 outlook = win32com.client.Dispatch('outlook.application')
 
-with open('name_list.csv', mode='r', newline='') as f:
-        file = f.read()
-file = file.splitlines()
+file = pd.read_csv('name_list.csv')
 
-# use the code below to change certain elements in email message, use a for loop
-
-
-
-
-
-def create_mail(emailadress, subject, message):
+# input paths to attachment_files as args, use path from source
+def create_mail(emailadress, subject, message, *args):
     mail = outlook.CreateItem(0)
     mail.To = emailadress
     mail.Subject = subject
     mail.HTMLBody = message
+    if args:
+        attachment_paths = list(args)
+        for i in attachment_paths:
+            mail.Attachments.Add(i)
     return mail
 
+# creates message by substituting required fields
 def subsitute_message(pahttohtml, name):
     html = Template(Path(pahttohtml).read_text())
     html = html.substitute({'name': name})
     return html
 
-
-for i in file:
-    print(i)
-    if i != 'Name':
-        html = subsitute_message('outlook_emails\\body_with_placeholder.html', i)
-        mail = create_mail('email@gmail.com', 'Sample', html)
-        mail.Display()
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     print(html)
-#     try:
-#         mail.Display()
-#         # or mail.Send()
-#         print('All good!')
-#     except:
-#         print('error!')
+if __name__ == '__main__':
+    for i in file.values:
+        try:
+            html = subsitute_message('outlook_emails\\formatted_body.html', i[0])
+            mail = create_mail(i[1], 'Sample', html)
+            mail.Display()
+        except ValueError as err:
+            print(err)
+        except TypeError as err:
+            print(err)
+        except IndexError as err:
+            print(err)
