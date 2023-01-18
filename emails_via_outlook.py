996561048761769dd119fd8a2a800f637d2a7092
Jamesmaster3 <120321282+Jamesmaster3@users.noreply.github.com>
2023-01-18 13:37:11 +0100

    Fixed empty attachment bug

--- emails_via_outlook.py
+++ emails_via_outlook.py
@@ -6,41 +6,47 @@ import pathlib
 
 outlook = win32com.client.Dispatch('outlook.application')
 
-file = pd.read_csv('bijlagen\\name_list.csv')
+file = pd.read_csv('name_list.csv')
 file = file.fillna(0)
 
 # Create email object and add email, CC emails, subject, message and (multiple) attachments [input path to directory]
+
+
 def create_mail(email_to, subject, message, attachment_dir=None, email_cc=None):
     mail = outlook.CreateItem(0)
     mail.To = email_to
     mail.Subject = subject
     mail.HTMLBody = message
 
-    if attachment_dir: #check if attachment is added
+    if attachment_dir:  # check if attachment is added
         # create absolute paths for files and loops trough them to add to mail object
-        p = pathlib.Path(attachment_dir).absolute() 
+        p = pathlib.Path(attachment_dir).absolute()
         attachments = list(p.iterdir())
         for child in attachments:
             print(child)
-        mail.Attachments.Add(str(child))     
+            mail.Attachments.Add(str(child))
 
     if email_cc:
-        if str(email_cc): #check if value in CC is not empty
+        if str(email_cc):  # check if value in CC is not empty
             mail.CC = email_cc
 
     return mail
 
 # creates message by substituting required fields
+
+
 def subsitute_message(pahttohtml, name):
     html = Template(Path(pahttohtml).read_text())
     html = html.substitute({'name': name})
     return html
 
+
 if __name__ == '__main__':
     for i in file.values:
         try:
             html = subsitute_message('formatted_body.html', i[0])
-            mail = create_mail(email_to=i[1], subject='Sample', message=html, attachment_dir='bijlagen', email_cc=i[2])
+            mail = create_mail(
+                email_to=i[1], subject='Sample', message=html, attachment_dir='bijlagen', email_cc=i[2])
             mail.Display()
 
         except ValueError as err:
